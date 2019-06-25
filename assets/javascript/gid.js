b65091a1cd41fee6f6de797b0cd0d3f08f7e097e
Zebadiah Miller <Zebadiahmiller@homtial.com>
2019-06-25 15:55:28 -0700

    buttons now intitially run but are able to be stopped and restarted with a click

--- assets/javascript/gid.js
+++ assets/javascript/gid.js
@@ -1,6 +1,6 @@
 // initial variable to hold the array of video games to get the giphs from
 
-const videoGames = ["Borderlands","Halo","Ratchet and Clank","Knights of the Old republic", "Mario", "Zelda", "God of War", "Sonic", "Final Fantasy"]
+const videoGames = ["Borderlands", "Halo", "Ratchet and Clank", "Knights of the Old republic", "Mario", "Zelda", "God of War", "Sonic", "Final Fantasy"]
 
 
 
@@ -8,54 +8,59 @@ const videoGames = ["Borderlands","Halo","Ratchet and Clank","Knights of the Old
 
 //giphy api calling
 
-function videoGameInfo(){
-    const games =$(this).attr("data-name");
+function videoGameInfo() {
+    const games = $(this).attr("data-name");
     const queryURL = "http://api.giphy.com/v1/gifs/search?q=" + games + "&api_key=5yEKIW6ooVIKYuOOaKgjSpYJGlXOSg6V&limit=10&offset=0&rating=r&lang=en";
     console.log(games);
-    
+
     //performing the ajax request
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-    //using .then to wait for the infor to come from the api
-    .then(function(response){
-        console.log(queryURL);
-        //logging the respons
-        console.log(response);
-        //
-        // $("#gifs-appear-here").text(JSON.stringify(response));
-        const results = response.data
+        //using .then to wait for the infor to come from the api
+        .then(function (response) {
+            console.log(queryURL);
+            //logging the respons
+            console.log(response);
+            //
+            // $("#gifs-appear-here").text(JSON.stringify(response));
+            const results = response.data
 
-        for (let r = 0; r < results.length; r++){
+            for (let r = 0; r < results.length; r++) {
 
-            
-            //making a div to hold the class of video-games
-            const videoGameDiv = $("<div class = 'video-games'>");
-            
-            // storing the rating data for the games gif
-            
-            
-            // element to store the rating of the giph
-            const paragraphOne =$("<p>"). text("Rating:" + results[r].rating);
-            //displaying rating
-            
-            // showing the gif
-            const gameImage =$("<img>");
-            
-            //creating the image tag
-            gameImage.attr("src", results[r].images.fixed_height.url);
-            
-            
-            //appending the gifs
-            videoGameDiv.append(paragraphOne);
-            videoGameDiv.append(gameImage);
-            // putting the gifs before the other gifs
-            $("#gifs-appear-here").prepend(videoGameDiv);
-        }
-        
-    });
+
+                //making a div to hold the class of video-games
+                const videoGameDiv = $("<div class = 'video-games'>");
+
+                // storing the rating data for the games gif
+
+
+                // element to store the rating of the giph
+                const paragraphOne = $("<p>").text("Rating:" + results[r].rating);
+                //displaying rating
+
+                // showing the gif
+                const gameImage = $("<img class = 'gif-image'>");
+
+                //creating the image tag
+                gameImage.attr("src", results[r].images.fixed_height.url);
+
+
+                //appending the gifs
+                videoGameDiv.append(paragraphOne);
+                videoGameDiv.append(gameImage);
+                // putting the gifs before the other gifs
+                $("#gifs-appear-here").prepend(videoGameDiv);
+
+                   
     
+                
+            }
+            
+
+        });
+
 }
 
 
@@ -63,13 +68,13 @@ function videoGameInfo(){
 
 
 //function to make the new buttons
-function makeButtons(){
-//clearing the buttons to not make repeat buttons
+function makeButtons() {
+    //clearing the buttons to not make repeat buttons
     $("#buttons-view").empty();
-//for loop to generate buttons from our array
-    for (let i = 0; i < videoGames.length; i++){
+    //for loop to generate buttons from our array
+    for (let i = 0; i < videoGames.length; i++) {
         //using jquery to make a new button in the html
-        const gameButtons =$("<button>");
+        const gameButtons = $("<button>");
 
         //adding a new class
         gameButtons.addClass("video-game-btn");
@@ -83,16 +88,16 @@ function makeButtons(){
         $("#buttons-view").append(gameButtons)
 
 
-        
+
     }
 };
 
 //add on click to add new button when typed into form
-$("#add-game").on("click", function(event){
+$("#add-game").on("click", function (event) {
     //prventing game from adding the initial loadup trying to submit the button itself before someone clicks
     event.preventDefault();
     //taking the input from the form
-    const games =$("#game-input").val().trim();
+    const games = $("#game-input").val().trim();
     //adding the new text into the array
 
     videoGames.push(games);
@@ -101,7 +106,24 @@ $("#add-game").on("click", function(event){
     makeButtons();
 
 });
-$(document).on("click", ".video-game-btn", videoGameInfo);
+function startStop(){
+
+    $(document).on("click", ".video-game-btn", videoGameInfo,);
+       
+    $('body').on('click', '.gif-image', function() {
+    	var src = $(this).attr("src");
+      if($(this).hasClass('playing')){
+         //stop
+         $(this).attr('src', src.replace(/\.gif/i, "_s.gif"))
+         $(this).removeClass('playing');
+      } else {
+        //play
+        $(this).addClass('playing');
+        $(this).attr('src', src.replace(/\_s.gif/i, ".gif"))
+      }
+    });
+}
+startStop();
 makeButtons();
 
 
